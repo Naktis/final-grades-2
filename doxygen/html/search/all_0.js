1df491717cd5cdc3e79f51c607c0ef3e3129e6f7
Augustina <37456430+Naktis@users.noreply.github.com>
2020-05-12 20:38:43 +0300

    Add Doxygen documentation

---
+++ doxygen/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['galutinių_20studentų_20balų_20skaičiuoklė_0',['Galutinių studentų balų skaičiuoklė',['../md__r_e_a_d_m_e.html',1,'']]]
+];
---
+++ doxygen/html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "gpst",
+  1: "pst",
+  2: "g"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Pages"
+};
+
